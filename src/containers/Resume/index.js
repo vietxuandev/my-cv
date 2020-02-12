7dfa97a89b9d14415457bb0f7c0c338459994b6c
Viet Xuan <vietxuandev@gmail.com>
2020-02-12 17:29:56 +0700

    update

--- src/containers/Resume/index.js
+++ src/containers/Resume/index.js
@@ -38,11 +38,8 @@ const Resume = () => {
                 Certified Network Security Center (CNSC)
               </span>
               <h4 className='item-title'>Interns</h4>
-              <p>- Together with the lecturers research thesis topics</p>
-              <p>
-                - There is the experience of the project using Django and Flask
-                to write API
-              </p>
+              <p>- Experienced in doing research with lectures</p>
+              <p>- Experienced in using Django and Flask to write API</p>
             </div>
             <div className='timeline-item clearfix'>
               <h5 className='item-period '>JULY 2018 - Now</h5>
@@ -61,7 +58,7 @@ const Resume = () => {
               <h5 className='item-period '>OCT 2019- DEC 2019</h5>
               <span className='item-company'>Beesight Soft</span>
               <h4 className='item-title'>Fresher Reactjs</h4>
-              <p>- Be involved in projects with Reactjs.</p>
+              <p>- Co-ordinated with ReactJS in doing projects</p>
             </div>
           </div>
           <div className=' col-xs-12 col-sm-12'>
@@ -76,9 +73,9 @@ const Resume = () => {
               </p>
               <p>- Framework: ReactJS, Django, Flask.</p>
               <p>- Platform: Nodejs.</p>
-              <p>- Having knowledge of information security.</p>
-              <p>- Good teamwork skills.</p>
-              <p>- Quickly absorb new technologies.</p>
+              <p>- Knowledgedgeable in information security.</p>
+              <p>- Skilled in teamwork.</p>
+              <p>- Dynamic in adopting new technology into working.</p>
             </div>
           </div>
         </div>
