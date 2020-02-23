02a733723670245e45595af0f5688b2c798beb6d
Viet Xuan <vietxuandev@gmail.com>
2020-02-23 14:36:33 +0700

    update cv

--- src/containers/About/index.js
+++ src/containers/About/index.js
@@ -1,7 +1,7 @@
 import React, { useState } from 'react';
 import './style.scss';
 import bangdiem from '../../files/bangdiem.pdf';
-import mycv from '../../files/my-cv.pdf';
+import cv from '../../files/cv.pdf';
 import { Modal, ModalBody } from 'reactstrap';
 const About = () => {
   const [viewPDF, setViewPDF] = useState(false);
@@ -45,7 +45,7 @@ const About = () => {
               </div>
               <div className='home-buttons'>
                 <a
-                  href={mycv}
+                  href={cv}
                   className='btn btn-primary download-button'
                   download
                 >
